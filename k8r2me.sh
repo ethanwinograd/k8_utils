03257f7eaebf4e175bea75412d1804dbf09f2421
Ethan Winograd <ethanwinograd@gmail.com>
2015-12-22 11:55:28 -0500

    cleanup

--- k8r2me.sh
+++ k8r2me.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-task=$1
-svc_name=$2
+function usage(){
+	echo "usage : 'k8r2me bash myservice' or 'k8r2me logs myservice'"
+}
 
 #arg1 is podname
 function k8_logs_cmd(){
@@ -15,19 +16,30 @@ function k8_bash_cmd(){
 }
 
 
+function validate(){
+	if [ "${task}" !=  "bash" ] && [ "${task}" !=  "logs" ]; then
+		usage
+		exit
+	fi
+
+	if [ -z "$svc_name" ]; then
+		usage
+		exit
+	fi
+}
+
+task=$1
+svc_name=$2
+
+validate
 number_of_pods=$(kubectl get pod|grep $svc_name|wc -l)
 if [ ${number_of_pods} -gt 1 ]; then
 	#spawn new terminals
 	vertical=300
-	#kubectl get pod|grep $svc_name| awk '{print $1;}'
 	for pod in $(kubectl get pod|grep $svc_name| awk '{print $1;}' )
 	do  
-		#echo ${pod}
 		let "vertical+=50"
-		#cmd="kubectl logs -f ${pod}"
-		#x-terminal-emulator --new-tab --title=${pod} --geometry=900x450+100+${vertical} -x ${cmd} &
 		if [ "${task}" =  "bash" ]; then
-			echo ${task}
 			x-terminal-emulator --new-tab --title=${pod} --geometry=900x450+100+${vertical} -x kubectl exec -it ${pod} bash &
 		else
 			x-terminal-emulator --new-tab --title=${pod} --geometry=900x450+100+${vertical} -x kubectl logs -f ${pod} &
@@ -37,7 +49,6 @@ else
 	#use this terminal
 	pod=$(kubectl get pod|grep ${svc_name}| awk '{print $1;}' )
 	if [ "${task}" = "bash" ]; then
-		echo ${task}
 		k8_bash_cmd ${pod}
 	else
 		k8_logs_cmd ${pod}
